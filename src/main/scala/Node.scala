ec21572d5ab5dfa9065e53279eb51f2af375dfdf
rafaelkyrdan <rafaelkyrdan@gmail.com>
2016-06-19 11:58:04 +0300

    Refactoring the Node Type

--- src/main/scala/Node.scala
+++ src/main/scala/Node.scala
@@ -1,22 +1,24 @@
 package main.scala
 
+import com.sun.javaws.security.AppPolicy
+
 /**
   * Class which represents node in the graph
   * Method add used to build references between nodes.
   */
 
 
-class Node(val n: Int) {
+class Node(private val n: Int) {
+
+  //leave these public for now because we need access them:
+  //we need access to @variable refs to traverse children in the `count` function
+  //we need access to @variable graphId because at the moment instantiation
+  // of the object we don't know parent's id
   var refs: List[Int] = List()
   var graphId = 0
 
   def add(x: Int) = {
-    this.refs = x :: refs
-  }
-
-  def apply(n: Int): Node = {
-    var id = n
-    new Node(n)
+    refs = x :: refs
   }
 
   override def toString = s"Node(id = $n, graphId = $graphId, references = $refs)"
